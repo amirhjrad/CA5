d4c61f7f9137717e9f3f5fa0872a0ef77ca21abf
hesam <a.h.jafarirad@gmail.com>
2023-11-27 12:18:38 +0330

    modified:   test/main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,8 +8,7 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 const float PLAYER_SPEED = 200.0f;
 
-class Player
-{
+class Player {
 public:
     Player(){
         shape.setSize(sf::Vector2f(50.0f, 50.0f));
@@ -32,10 +32,7 @@ public:
 
     sf::FloatRect getBounds() { return shape.getGlobalBounds(); }
 
-    void draw(sf::RenderWindow& window)
-    {
-        window.draw(shape);
-    }
+    void draw(sf::RenderWindow& window) { window.draw(shape); }
 
     void setTexture(string direction)
     {
@@ -57,14 +54,59 @@ private:
     sf::Texture textureRight;
 };
 
+
+class Bomb {
+public:
+    Bomb()
+    {
+        shape.setSize(sf::Vector2f(50.0f, 50.0f));
+        loadTexture();
+    }
+
+    void loadTexture()
+    {
+        texture.loadFromFile("assets/bomb.png");
+        shape.setTexture(&texture);
+    }
+
+    void draw(sf::RenderWindow& window) { window.draw(shape); }
+    void setPosition(sf::Vector2f position) { shape.setPosition(position); }
+
+private:
+    sf::RectangleShape shape;
+    sf::Texture texture;
+};
+
+
+
 class Game
 {
 public:
-    Game() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Game") {}
+    Game() : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Game") {
+        loadTexture();
+    }
+        
+    void drawTexture(){
+        int numTilesX = WINDOW_WIDTH / 50;
+            int numTilesY = WINDOW_HEIGHT / 50;
 
-    void run()
-    {
-        initialize();
+            // Draw grass tiles to cover the window
+            for (int i = 0; i < numTilesX; ++i) {
+                for (int j = 0; j < numTilesY; ++j) {
+                    sf::Sprite grassSprite;
+                    grassSprite.setTexture(textureGrass);
+                    grassSprite.setTextureRect(sf::IntRect(0, 0, 50, 50));  // Set the texture rectangle size to 50x50
+                    grassSprite.setPosition(i * 50, j * 50);  // Set the position based on the tile size
+                    window.draw(grassSprite);
+                }
+            }
+    }
+
+    void loadTexture(){
+        textureGrass.loadFromFile("assets/grass.png");
+    }
+    
+    void run() {
         sf::Clock clock;
         while (window.isOpen())
         {
@@ -111,12 +153,30 @@ public:
         }
     }
 
-private:
-    void initialize()
+    void handleBombPlacement()
     {
-        Player player;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+        {
+            sf::Vector2f playerPosition = player.getPosition();
+
+            // Calculate the grid position within the grass area
+            int gridX = static_cast<int>(playerPosition.x + player.getBounds().width / 2) / 50;
+            int gridY = static_cast<int>(playerPosition.y + player.getBounds().height / 2) / 50;
+
+            // Calculate the position within the grass area
+            float positionX = (gridX * 50);
+            float positionY = (gridY * 50);
+
+            // Set the bomb only if the center of the player position is within the grass area
+            if (gridX >= 0 && gridX < (WINDOW_WIDTH / 50) && gridY >= 0 && gridY < (WINDOW_HEIGHT / 50))
+            {
+                tmpBomb.setPosition(sf::Vector2f(positionX, positionY));
+                bombs.push_back(tmpBomb);
+            }
+        }
     }
 
+private:
     void processEvents()
     {
         sf::Event event;
@@ -130,23 +190,30 @@ private:
     void update(sf::Time deltaTime)
     {
         handlePlayerMovement(deltaTime);
+        handleBombPlacement();
     }
 
     void render()
     {
         window.clear();
+        drawTexture();
         player.draw(window);
+        for(Bomb &tmpBomb : bombs){
+            tmpBomb.draw(window);
+        }
         window.display();
     }
 
+    Bomb tmpBomb;
     sf::RenderWindow window;
     Player player;
+    vector<Bomb> bombs;
+    sf::Texture textureGrass;
 };
 
 int main()
 {
     Game game;
     game.run();
-
     return 0;
 }
