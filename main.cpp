96325d21bcd7702124cbb513bbe7ff3b60aef9ea
maryam_zolghadr <zolghadrmaryam2004@gmail.com>
2023-12-06 18:23:37 +0330

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -475,76 +475,115 @@ void placeKey(vector<vector<char>>& mapData)
             render();
         }
     }
-void handleEnemyMovement(sf::Time deltaTime)
+void handleEnemyMovement(Enemy& enemy)
 {
-    for(Enemy &enemy : enemies)
-    {
-        obstacleCollision = false;
-        sf::Vector2f movement(0.0f, 0.0f);
-        if(!obstacleCollision)
-        {
-            if (enemy.upOrRight)
-            {
-                if(enemy.isVertical)
-                    movement.y -= ENEMY_SPEED * deltaTime.asSeconds();
-                else
-                    movement.x += ENEMY_SPEED * deltaTime.asSeconds();
-            }
-            else
-            {
-                if(enemy.isVertical)
-                    movement.y += ENEMY_SPEED * deltaTime.asSeconds();
-                else
-                    movement.x -= ENEMY_SPEED * deltaTime.asSeconds();               
-            }
+    const float ENEMY_SPEED = 5.0f;
+    sf::Vector2f movement(0.0f, 0.0f);
+
+    if (enemy.isVertical) {
+        if (enemy.upOrRight) {
+            movement.y -= ENEMY_SPEED;
         }
-        else
-        {
-            if (enemy.upOrRight)
-            {
-                if(enemy.isVertical)
-                    movement.y += ENEMY_SPEED * deltaTime.asSeconds();
-                else
-                    movement.x -= ENEMY_SPEED * deltaTime.asSeconds();
-            }
-            else
-            {
-                if(enemy.isVertical)
-                    movement.y -= ENEMY_SPEED * deltaTime.asSeconds();
-                else
-                    movement.x += ENEMY_SPEED * deltaTime.asSeconds();               
-            }            
+        else {
+            movement.y += ENEMY_SPEED;
         }
-        
-        sf::Vector2f newPosition = enemy.getPosition() + movement;
-        sf::FloatRect enemyBounds = enemy.getBounds();
-        sf::FloatRect windowBounds(0.0f, 50.0f - 6, WINDOW_WIDTH, WINDOW_HEIGHT - 50.0f - 6);
-        sf::FloatRect newBounds(newPosition, sf::Vector2f(enemyBounds.width, enemyBounds.height));   
-        for (int i = 0; i < map.size(); ++i)
-        {
-            for (int j = 0; j < map[i].size(); ++j)
-            {
-                char c = map[i][j];
-                sf::Vector2f obstaclePosition(j * 50, i * 50);
-                sf::FloatRect obstacleBounds(obstaclePosition, sf::Vector2f(40.0f, 40.0f));
-                
-                if (c == 'B' || c == 'P')
-                {
-                    if (newBounds.intersects(obstacleBounds))
-                    {
-                        obstacleCollision = true;
-                        break;
-                    }
-                }
-            }
-            
-            if (obstacleCollision)
-                break;
-        }
-        
-        enemy.move(newPosition);
     }
+    else {
+        if (enemy.upOrRight) {
+            movement.x += ENEMY_SPEED;
+        }
+        else {
+            movement.x -= ENEMY_SPEED;
+        }
+    }
+
+    // Check for collision with obstacles
+    sf::FloatRect enemyBounds = enemy.getBounds();
+    int x = static_cast<int>(enemyBounds.left / 50);
+    int y = static_cast<int>(enemyBounds.top / 50);
+
+    if (map[y][x] == 'B') {
+        if (enemy.isVertical) {
+            enemy.upOrRight = !enemy.upOrRight;
+        }
+        else {
+            enemy.upOrRight = !enemy.upOrRight;
+        }
+    }
+
+    enemy.move(movement);
 }
+
+// void handleEnemyMovement(sf::Time deltaTime)
+// {
+//     for(Enemy &enemy : enemies)
+//     {
+//         obstacleCollision = false;
+//         sf::Vector2f movement(0.0f, 0.0f);
+//         if(!obstacleCollision)
+//         {
+//             if (enemy.upOrRight)
+//             {
+//                 if(enemy.isVertical)
+//                     movement.y -= ENEMY_SPEED * deltaTime.asSeconds();
+//                 else
+//                     movement.x += ENEMY_SPEED * deltaTime.asSeconds();
+//             }
+//             else
+//             {
+//                 if(enemy.isVertical)
+//                     movement.y += ENEMY_SPEED * deltaTime.asSeconds();
+//                 else
+//                     movement.x -= ENEMY_SPEED * deltaTime.asSeconds();               
+//             }
+//         }
+//         else
+//         {
+//             if (enemy.upOrRight)
+//             {
+//                 if(enemy.isVertical)
+//                     movement.y += ENEMY_SPEED * deltaTime.asSeconds();
+//                 else
+//                     movement.x -= ENEMY_SPEED * deltaTime.asSeconds();
+//             }
+//             else
+//             {
+//                 if(enemy.isVertical)
+//                     movement.y -= ENEMY_SPEED * deltaTime.asSeconds();
+//                 else
+//                     movement.x += ENEMY_SPEED * deltaTime.asSeconds();               
+//             }            
+//         }
+        
+//         sf::Vector2f newPosition = enemy.getPosition() + movement;
+//         sf::FloatRect enemyBounds = enemy.getBounds();
+//         sf::FloatRect windowBounds(0.0f, 50.0f - 6, WINDOW_WIDTH, WINDOW_HEIGHT - 50.0f - 6);
+//         sf::FloatRect newBounds(newPosition, sf::Vector2f(enemyBounds.width, enemyBounds.height));   
+//         for (int i = 0; i < map.size(); ++i)
+//         {
+//             for (int j = 0; j < map[i].size(); ++j)
+//             {
+//                 char c = map[i][j];
+//                 sf::Vector2f obstaclePosition(j * 50, i * 50);
+//                 sf::FloatRect obstacleBounds(obstaclePosition, sf::Vector2f(40.0f, 40.0f));
+                
+//                 if (c == 'B' || c == 'P')
+//                 {
+//                     if (newBounds.intersects(obstacleBounds))
+//                     {
+//                         obstacleCollision = true;
+//                         break;
+//                     }
+//                 }
+//             }
+            
+//             if (obstacleCollision)
+//                 break;
+//         }
+        
+//         enemy.move(newPosition);
+//     }
+// }
     void handlePlayerMovement(sf::Time deltaTime)
     {
         sf::Vector2f movement(0.0f, 0.0f);
@@ -704,7 +743,10 @@ private:
             finished = true;
         }
         handlePlayerMovement(deltaTime);
-        handleEnemyMovement(deltaTime);
+        for(Enemy &enemy : enemies)
+        {
+            handleEnemyMovement(enemy);
+        }
         for (Bomb& bomb : bombs) {
             bomb.update(deltaTime);
             if (bomb.remove()) {
@@ -781,42 +823,5 @@ int main()
 }
 
 
-// void moveEnemy(Enemy& enemy, std::vector<std::vector<char>>& map)
-// {
-//     const float ENEMY_SPEED = 5.0f;
-//     sf::Vector2f movement(0.0f, 0.0f);
 
-//     if (enemy.isVertical) {
-//         if (enemy.upOrRight) {
-//             movement.y -= ENEMY_SPEED;
-//         }
-//         else {
-//             movement.y += ENEMY_SPEED;
-//         }
-//     }
-//     else {
-//         if (enemy.upOrRight) {
-//             movement.x += ENEMY_SPEED;
-//         }
-//         else {
-//             movement.x -= ENEMY_SPEED;
-//         }
-//     }
-
-//     // Check for collision with obstacles
-//     sf::FloatRect enemyBounds = enemy.getBounds();
-//     int x = static_cast<int>(enemyBounds.left / 50);
-//     int y = static_cast<int>(enemyBounds.top / 50);
-
-//     if (map[y][x] == 'B') {
-//         if (enemy.isVertical) {
-//             enemy.upOrRight = !enemy.upOrRight;
-//         }
-//         else {
-//             enemy.upOrRight = !enemy.upOrRight;
-//         }
-//     }
-
-//     enemy.move(movement);
-}
 
